90271058befbd691932aa1df0a6219a053aef644
Domitory1 <jomunyts@gmail.com>
2024-06-06 07:48:25 +0300

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(morgan('dev'));
 //app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
+	// Разрешаем все источники (*)
     res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
